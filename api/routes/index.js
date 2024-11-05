a4f922d300ba1ec0e80a59b2d774f049be627fb8
Deniz Akyol <denizbaran.akyol@gmail.com>
2024-11-05 11:30:04 +0300

    index.js update

--- api/routes/index.js
+++ api/routes/index.js
@@ -7,7 +7,7 @@ let routes = fs.readdirSync(__dirname)
 
 for (let route of routes) {
   if (route.includes(".js") && route != "index.js") {
-    router.use("/"+route.replace(".js",""), require("./"+route)) 
+    router.use("/" + route.replace(".js", ""), require("./" + route))
   }
 }
 
